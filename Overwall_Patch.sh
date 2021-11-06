e70d689872df88e7726e153aa4f97613af7be968
fangxx3863 <2221048564@qq.com>
2021-11-06 13:28:24 +0800

    Update

---
+++ Overwall_Patch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/eFw58nNRXXfTwU4#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/China_IPList#g' /usr/share/overwall/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/t8eOh94EJIHTXR6#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/China_IPv6List#g' /usr/share/overwall/update
+sed -i 's#https://cdn.jsdelivr.net/gh/yIwIoTT9A21nupT/2aXGmlWs/KkFCtZkeAP/avtPeqDKt645Arm#https://cdn.jsdelivr.net/gh/fangxx3863/overwall_list/GFW_List#g' /usr/share/overwall/update
+
+sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/eFw58nNRXXfTwU4#https://raw.githubusercontent.com/fangxx3863/overwall_list/main/China_IPList#g' /usr/share/overwall/update
+sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/t8eOh94EJIHTXR6#https://raw.githubusercontent.com/fangxx3863/overwall_list/main/China_IPv6List#g' /usr/share/overwall/update
+sed -i 's#https://raw.githubusercontent.com/yIwIoTT9A21nupT/2aXGmlWs/master/KkFCtZkeAP/avtPeqDKt645Arm#https://raw.githubusercontent.com/fangxx3863/overwall_list/main/GFW_List#g' /usr/share/overwall/update
+
